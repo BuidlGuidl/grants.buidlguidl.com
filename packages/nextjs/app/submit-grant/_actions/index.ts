41e59fb33ef85845944134cdf678372abcb86932
Shiv Bhonde <shivbhonde04@gmail.com>
2024-02-03 23:57:25 +0530

    add server actions for submiting grants form

---
+++ packages/nextjs/app/submit-grant/_actions/index.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import { verifyMessage } from "viem";
+import { findUserByAddress } from "~~/services/database/users";
+
+type SignedMessage = {
+  message?: string;
+  signature?: `0x${string}`;
+  address?: string;
+};
+
+export const subgmitGrantAction = async ({ message, signature, address }: SignedMessage, form: FormData) => {
+  try {
+    const formData = Object.fromEntries(form.entries());
+    if (!formData.title || !formData.description || !formData.askAmount) {
+      throw new Error("Invalid form data");
+    }
+    if (!message || !signature || !address) {
+      throw new Error("Invalid message, signature, or address.");
+    }
+
+    const isMessageValid = await verifyMessage({ message, signature, address });
+    if (!isMessageValid) {
+      throw new Error("Invalid signature");
+    }
+
+    // Verif if the builder is present
+    const builder = await findUserByAddress(address);
+    if (!builder.exists) {
+      throw new Error("Only buidlguild builders can submit for grants");
+    }
+
+    // Save the form data to the database
+  } catch (e) {
+    if (e instanceof Error) {
+      throw e;
+    }
+
+    throw new Error("Error processing form");
+  }
+};
--- packages/nextjs/app/submit-grant/_component/Form.tsx
+++ packages/nextjs/app/submit-grant/_component/Form.tsx
@@ -1,70 +1,73 @@
 "use client";
 
-import { useState } from "react";
+import { subgmitGrantAction } from "../_actions";
+import SubmitButton from "./SubmitButton";
 import { useAccount, useSignMessage } from "wagmi";
-import { InputBase } from "~~/components/scaffold-eth";
-import { useBGBuilderData } from "~~/hooks/useBGBuilderData";
 import { notification } from "~~/utils/scaffold-eth";
-
-/*
- * 1. Disable submit button when wallet is not connected, and show a tooltip
- * 2. Fetch for bg status once the wallet connected
- */
 
 const selectOptions = [0.1, 0.25, 0.5, 1];
 
 const Form = () => {
-  const [formState, setFormState] = useState({ title: "", description: "", askAmount: 0.1 });
   const { signMessageAsync } = useSignMessage();
-  const { isConnected, address: connectedAddress } = useAccount();
-  const { isBuilderPresent, isLoading: isFetchingBuilderData } = useBGBuilderData(connectedAddress);
+  const { address: connectedAddress } = useAccount();
 
-  const handleSubmit = async () => {
-    if (formState.title === "" || formState.description === "") {
-      notification.error("Title and description are required");
-      return;
+  const clientFormAction = async (formData: FormData) => {
+    try {
+      const formState = Object.fromEntries(formData.entries());
+      if (formState.title === "" || formState.description === "") {
+        notification.error("Title and description are required");
+        return;
+      }
+
+      const message = JSON.stringify(formState);
+      const signature = await signMessageAsync({ message });
+      const signedMessageObject = {
+        signature: signature,
+        address: connectedAddress,
+        message,
+      };
+
+      // server action
+      const submitGrantActionWithSignedMessage = subgmitGrantAction.bind(null, signedMessageObject);
+      await submitGrantActionWithSignedMessage(formData);
+    } catch (error: any) {
+      if (error instanceof Error) {
+        notification.error(error.message);
+        return;
+      }
+      notification.error("Something went wrong");
     }
-
-    const signedMessage = await signMessageAsync({ message: JSON.stringify(formState) });
-
-    console.log("Signed message", signedMessage);
-    console.log("Form data", formState);
   };
-
-  const isSubmitDisabled = !isConnected || isFetchingBuilderData || !isBuilderPresent;
 
   return (
     <div className="card card-compact w-96 bg-base-100 shadow-xl">
-      <div className="card-body space-y-4">
+      <form action={clientFormAction} className="card-body space-y-4">
         <h2 className="card-title self-center text-3xl !mb-0">Submit Proposal</h2>
         <div className="space-y-2">
           <p className="m-0 text-xl ml-2">Title</p>
-          <InputBase
-            placeholder="title"
-            value={formState.title}
-            onChange={value => setFormState({ ...formState, title: value })}
-          />
+          <div className="flex border-2 border-base-300 bg-base-200 rounded-full text-accent">
+            <input
+              className="input input-ghost focus-within:border-transparent focus:outline-none focus:bg-transparent focus:text-gray-400 h-[2.2rem] min-h-[2.2rem] px-4 border w-full font-medium placeholder:text-accent/50 text-gray-400"
+              placeholder="title"
+              name="title"
+              autoComplete="off"
+            />
+          </div>
         </div>
-        {/* TODO: Probably we want this to WYSIWYG editor */}
         <div className="space-y-2">
           <p className="m-0 text-xl ml-2">Description</p>
           <div className="flex border-2 border-base-300 bg-base-200 rounded-3xl text-accent">
             <textarea
               className="input input-ghost focus-within:border-transparent focus:outline-none focus:bg-transparent focus:text-gray-400 px-4 pt-2 border w-full font-medium placeholder:text-accent/50 text-gray-400 h-28 rounded-none"
               placeholder="description"
-              value={formState.description}
-              onChange={e => setFormState({ ...formState, description: e.target.value })}
+              name="description"
               autoComplete="off"
             />
           </div>
         </div>
         <div className="space-y-2">
           <p className="m-0 text-xl ml-2">Ask amount</p>
-          <select
-            className="select bg-base-200 select-primary select-md select-bordered w-full"
-            value={formState.askAmount}
-            onChange={e => setFormState({ ...formState, askAmount: parseFloat(e.target.value) })}
-          >
+          <select className="select bg-base-200 select-primary select-md select-bordered w-full" name="askAmount">
             <option disabled>Select amount</option>
             {selectOptions.map(option => (
               <option key={option} value={option}>
@@ -73,17 +76,8 @@ const Form = () => {
             ))}
           </select>
         </div>
-
-        <div
-          className={`flex ${isSubmitDisabled && "tooltip tooltip-bottom"}`}
-          data-tip={`${!isConnected ? "Please connect your wallet" : !isBuilderPresent ? "Builder not found" : ""}`}
-        >
-          <button className="btn btn-primary w-full" disabled={isSubmitDisabled} onClick={handleSubmit}>
-            {isFetchingBuilderData && <span className="loading loading-spinner loading-md"></span>}
-            Submit
-          </button>
-        </div>
-      </div>
+        <SubmitButton />
+      </form>
     </div>
   );
 };
