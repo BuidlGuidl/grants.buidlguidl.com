25a0631aebd7903c49451fb546810f2c1f14fbab
Shiv Bhonde <shivbhonde04@gmail.com>
2024-02-04 15:32:05 +0530

    add basic error handling in GET route

--- packages/nextjs/app/api/builders/[builderAddress]/route.ts
+++ packages/nextjs/app/api/builders/[builderAddress]/route.ts
@@ -2,8 +2,16 @@ import { NextResponse } from "next/server";
 import { findUserByAddress } from "~~/services/database/users";
 
 export async function GET(_request: Request, { params }: { params: { builderAddress: string } }) {
-  const builderAddress = params.builderAddress;
-  const builderData = await findUserByAddress(builderAddress);
-
-  return NextResponse.json(builderData);
+  try {
+    const builderAddress = params.builderAddress;
+    const builderData = await findUserByAddress(builderAddress);
+    return NextResponse.json(builderData);
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      {
+        status: 500,
+      },
+    );
+  }
 }
