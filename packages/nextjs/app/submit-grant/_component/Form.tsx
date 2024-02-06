2d0bea5c674e73c1f8ae1bc90c290e8bf59e26d4
Shiv Bhonde | shivbhonde.eth <shivbhonde04@gmail.com>
2024-02-06 20:03:51 +0530

    Submit grants page (#8)

---
+++ packages/nextjs/app/submit-grant/_component/Form.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { submitGrantAction } from "../_actions";
+import SubmitButton from "./SubmitButton";
+import { useAccount, useSignMessage } from "wagmi";
+import { notification } from "~~/utils/scaffold-eth";
+
+const selectOptions = [0.1, 0.25, 0.5, 1];
+
+const Form = () => {
+  const { signMessageAsync } = useSignMessage();
+  const { address: connectedAddress } = useAccount();
+  const router = useRouter();
+
+  const clientFormAction = async (formData: FormData) => {
+    try {
+      const formState = Object.fromEntries(formData.entries());
+      if (formState.title === "" || formState.description === "") {
+        notification.error("Title and description are required");
+        return;
+      }
+
+      const signature = await signMessageAsync({ message: JSON.stringify(formState) });
+      const signedMessageObject = {
+        signature: signature,
+        address: connectedAddress,
+      };
+
+      // server action
+      const submitGrantActionWithSignedMessage = submitGrantAction.bind(null, signedMessageObject);
+      await submitGrantActionWithSignedMessage(formData);
+
+      notification.success("Proposal submitted successfully!");
+      router.push("/");
+    } catch (error: any) {
+      if (error instanceof Error) {
+        notification.error(error.message);
+        return;
+      }
+      notification.error("Something went wrong");
+    }
+  };
+
+  return (
+    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+      <form action={clientFormAction} className="card-body space-y-4">
+        <h2 className="card-title self-center text-3xl !mb-0">Submit Proposal</h2>
+        <div className="space-y-2">
+          <p className="m-0 text-xl ml-2">Title</p>
+          <div className="flex border-2 border-base-300 bg-base-200 rounded-full text-accent">
+            <input
+              className="input input-ghost focus-within:border-transparent focus:outline-none focus:bg-transparent focus:text-gray-400 h-[2.2rem] min-h-[2.2rem] px-4 border w-full font-medium placeholder:text-accent/50 text-gray-400"
+              placeholder="title"
+              name="title"
+              autoComplete="off"
+            />
+          </div>
+        </div>
+        <div className="space-y-2">
+          <p className="m-0 text-xl ml-2">Description</p>
+          <div className="flex border-2 border-base-300 bg-base-200 rounded-3xl text-accent">
+            <textarea
+              className="input input-ghost focus-within:border-transparent focus:outline-none focus:bg-transparent focus:text-gray-400 px-4 pt-2 border w-full font-medium placeholder:text-accent/50 text-gray-400 h-28 rounded-none"
+              placeholder="description"
+              name="description"
+              autoComplete="off"
+            />
+          </div>
+        </div>
+        <div className="space-y-2">
+          <p className="m-0 text-xl ml-2">Ask amount</p>
+          <select className="select bg-base-200 select-primary select-md select-bordered w-full" name="askAmount">
+            <option disabled>Select amount</option>
+            {selectOptions.map(option => (
+              <option key={option} value={option}>
+                {option} ETH
+              </option>
+            ))}
+          </select>
+        </div>
+        <SubmitButton />
+      </form>
+    </div>
+  );
+};
+
+export default Form;
---
+++ packages/nextjs/app/submit-grant/_component/SubmitButton.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+import { useAccount } from "wagmi";
+import { useBGBuilderData } from "~~/hooks/useBGBuilderData";
+
+// To use useFormStatus we need to make sure button is child of form
+const SubmitButton = () => {
+  const { pending } = useFormStatus();
+  const { isConnected, address: connectedAddress } = useAccount();
+  const { isBuilderPresent, isLoading: isFetchingBuilderData } = useBGBuilderData(connectedAddress);
+  const isSubmitDisabled = !isConnected || isFetchingBuilderData || pending || !isBuilderPresent;
+
+  return (
+    <div
+      className={`flex ${(!isConnected || !isBuilderPresent) && "tooltip tooltip-bottom"}`}
+      data-tip={`${
+        !isConnected
+          ? "Please connect your wallet"
+          : !isBuilderPresent
+          ? "You should be a buidlguidl builder to submit grant"
+          : ""
+      }`}
+    >
+      <button className="btn btn-primary w-full" disabled={isSubmitDisabled} aria-disabled={isSubmitDisabled}>
+        {(isFetchingBuilderData || pending) && <span className="loading loading-spinner loading-md"></span>}
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default SubmitButton;
