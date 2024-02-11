7a09c9a2abd640c6b55b7b758f90dbe597a904ef
Shiv Bhonde <shivbhonde04@gmail.com>
2024-02-11 23:10:26 +0530

    add sparkle for title

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -7,9 +7,18 @@ const Home = () => {
       <div>
         <div className="container max-w-[90%] lg:max-w-6xl m-auto py-10 lg:py-16 xl:pl-18 lg:pl-8 flex flex-col-reverse lg:flex-row items-center justify-between gap-5 lg:gap-0">
           <div className="space-y-6 lg:max-w-[60%] flex flex-col items-center lg:items-start">
-            <h2 className="text-2xl lg:text-4xl xl:text-5xl text-center lg:text-left">
-              Funding meaningful projects across the ecosystem
-            </h2>
+            <div className="relative">
+              <h2 className="text-2xl lg:text-4xl xl:text-5xl text-center lg:text-left">
+                Funding meaningful projects <br /> across the ecosystem
+              </h2>
+              <Image
+                className="absolute -top-3 -right-7"
+                src="/assets/sparkle.png"
+                alt="sparkle"
+                width={32}
+                height={32}
+              />
+            </div>
             <div className="text-center px-1 max-w-lg lg:max-w-none lg:w-3/5 lg:px-0 lg:text-left space-y-5">
               <p className="m-0">
                 Our micro grants are one of many ways BuidlGuidl funds development and contribute to Ethereum. Did you
