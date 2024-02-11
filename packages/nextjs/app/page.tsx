7726e674dcc139ae27f3e1a8c7fc7af2e946511f
Shiv Bhonde <shivbhonde04@gmail.com>
2024-02-11 16:46:45 +0530

    v0.1 hero section

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,26 +1,28 @@
-import { listCollections } from "~~/services/database/collections";
+import Image from "next/image";
 
-// revalidate the data at most every hour (just testing)
-export const revalidate = 3600;
-
-const Home = async () => {
-  // This happens on the server
-  const collections = await listCollections();
-
+const Home = () => {
   return (
     <>
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center mb-8">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">grants.buidlguidl.com</span>
-          </h1>
-          <h2 className="font-bold">Collections:</h2>
-          <ul>
-            {collections.map(collection => (
-              <li key={collection.id}>{collection.id}</li>
-            ))}
-          </ul>
+      {/* Hero section */}
+      <div>
+        <div className="container max-w-[90%] lg:max-w-6xl m-auto py-10 lg:py-16 xl:pl-18 lg:pl-8 flex flex-col-reverse lg:flex-row items-center justify-between gap-5 lg:gap-0">
+          <div className="space-y-6 lg:max-w-[60%] flex flex-col items-center lg:items-start">
+            <h2 className="text-2xl lg:text-4xl xl:text-5xl text-center lg:text-left">
+              Funding meaningful projects across the ecosystem
+            </h2>
+            <div className="text-center px-1 max-w-lg lg:max-w-none lg:w-3/5 lg:px-0 lg:text-left space-y-5">
+              <p className="m-0">
+                Our micro grants are one of many ways BuidlGuidl funds development and contribute to Ethereum. Did you
+                just finished SRE or completed one of our batches? This could be your next step in BuidlGuidl’s journey.
+              </p>
+              <button className="btn bg-base-100 btn-md h-14 px-16 font-medium">Learn More</button>
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="max-w-md lg:max-w-none">
+              <Image src="/assets/bg-universe.png" alt="developers list" width={500} height={500} />
+            </div>
+          </div>
         </div>
       </div>
     </>
--- packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton/index.tsx
+++ packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton/index.tsx
@@ -33,7 +33,7 @@ export const RainbowKitCustomConnectButton = () => {
               if (!connected) {
                 return (
                   <button
-                    className="btn btn-primary btn-md px-8 py-4 font-medium"
+                    className="btn btn-primary btn-md px-6 py-2 md:px-8 md:py-4 font-medium"
                     onClick={openConnectModal}
                     type="button"
                   >
