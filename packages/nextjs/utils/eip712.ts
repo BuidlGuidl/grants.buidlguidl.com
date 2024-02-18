2c0b5cb6e2a987c0be97bc8b09fbe6ae6f2a04fc
Carlos Sánchez <oceanrdn@gmail.com>
2024-02-18 21:42:02 +0100

    Revert back to OP

--- packages/nextjs/utils/eip712.ts
+++ packages/nextjs/utils/eip712.ts
@@ -1,7 +1,7 @@
 export const EIP_712_DOMAIN = {
   name: "BuidlGuidl Grants",
   version: "1",
-  chainId: 1,
+  chainId: 10,
 } as const;
 
 // ToDo. We could add more fields (grant title, builder, etc)
