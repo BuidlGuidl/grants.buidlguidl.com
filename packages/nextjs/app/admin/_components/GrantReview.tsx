ac034443432dadaddc8b87491d6c8abf397e22e2
Shiv Bhonde <shivbhonde04@gmail.com>
2024-02-20 11:53:55 +0530

    update admin with useSWR

---
+++ packages/nextjs/app/admin/_components/GrantReview.tsx
@@ -0,0 +1,108 @@
+import { useSWRConfig } from "swr";
+import useSWRMutation from "swr/mutation";
+import { useAccount, useSignTypedData } from "wagmi";
+import { GrantData } from "~~/services/database/schema";
+import { EIP_712_DOMAIN, EIP_712_TYPES__REVIEW_GRANT } from "~~/utils/eip712";
+import { PROPOSAL_STATUS, ProposalStatusType } from "~~/utils/grants";
+import { notification } from "~~/utils/scaffold-eth";
+
+const reviewGrant = async (
+  url: string,
+  { arg }: { arg: { signer: string; signature: `0x${string}`; action: ProposalStatusType } },
+) => {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(arg),
+  });
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.error || "Error reviewing grant");
+  }
+  return data;
+};
+
+export const GrantReview = ({ grant }: { grant: GrantData }) => {
+  const { address } = useAccount();
+  const { signTypedDataAsync, isLoading: isSigningMessage } = useSignTypedData();
+  const { trigger: postReviewGrant, isMutating: isPostingNewGrant } = useSWRMutation(
+    `/api/grants/${grant.id}/review`,
+    reviewGrant,
+  );
+  const { mutate } = useSWRConfig();
+  const isLoading = isSigningMessage || isPostingNewGrant;
+
+  const handleReviewGrant = async (grant: GrantData, action: ProposalStatusType) => {
+    if (!address) {
+      notification.error("Please connect your wallet");
+      return;
+    }
+
+    let signature;
+    try {
+      signature = await signTypedDataAsync({
+        domain: EIP_712_DOMAIN,
+        types: EIP_712_TYPES__REVIEW_GRANT,
+        primaryType: "Message",
+        message: {
+          grantId: grant.id,
+          action: action,
+        },
+      });
+    } catch (e) {
+      console.error("Error signing message", e);
+      notification.error("Error signing message");
+      return;
+    }
+
+    let notificationId;
+    try {
+      notificationId = notification.loading("Submiting reivew");
+      await postReviewGrant({ signer: address, signature, action });
+      await mutate("/api/grants/review");
+      notification.remove(notificationId);
+      notification.success(`Grant reviewed: ${action}`);
+    } catch (error) {
+      notification.error("Error reviewing grant");
+    } finally {
+      if (notificationId) notification.remove(notificationId);
+    }
+  };
+
+  if (grant.status !== PROPOSAL_STATUS.PROPOSED && grant.status !== PROPOSAL_STATUS.SUBMITTED) return null;
+
+  const acceptStatus = grant.status === PROPOSAL_STATUS.PROPOSED ? PROPOSAL_STATUS.APPROVED : PROPOSAL_STATUS.COMPLETED;
+  const acceptLabel = grant.status === PROPOSAL_STATUS.PROPOSED ? "Approve" : "Complete";
+  return (
+    <div className="border p-4 my-4">
+      <h3 className="font-bold">
+        {grant.title}
+        <span className="text-sm text-gray-500 ml-2">({grant.id})</span>
+      </h3>
+      <p>{grant.description}</p>
+      <div className="mt-4">
+        <button
+          className={`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ${
+            isLoading ? "opacity-50" : ""
+          }`}
+          onClick={() => handleReviewGrant(grant, acceptStatus)}
+          disabled={isLoading}
+        >
+          {acceptLabel}
+        </button>
+        <button
+          className={`bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-4 ${
+            isLoading ? "opacity-50" : ""
+          }`}
+          onClick={() => handleReviewGrant(grant, PROPOSAL_STATUS.REJECTED)}
+          disabled={isLoading}
+        >
+          Reject
+        </button>
+      </div>
+    </div>
+  );
+};
--- packages/nextjs/app/admin/page.tsx
+++ packages/nextjs/app/admin/page.tsx
@@ -1,119 +1,40 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { useAccount, useSignTypedData } from "wagmi";
+import { GrantReview } from "./_components/GrantReview";
+import useSWR from "swr";
 import { GrantData } from "~~/services/database/schema";
-import { EIP_712_DOMAIN, EIP_712_TYPES__REVIEW_GRANT } from "~~/utils/eip712";
-import { PROPOSAL_STATUS, ProposalStatusType } from "~~/utils/grants";
+import { PROPOSAL_STATUS } from "~~/utils/grants";
 import { notification } from "~~/utils/scaffold-eth";
 
-const GrantReview = ({
-  grant,
-  reviewGrant,
-}: {
-  grant: GrantData;
-  reviewGrant: (grant: GrantData, action: ProposalStatusType) => void;
-}) => {
-  if (grant.status !== PROPOSAL_STATUS.PROPOSED && grant.status !== PROPOSAL_STATUS.SUBMITTED) return null;
-
-  const acceptStatus = grant.status === PROPOSAL_STATUS.PROPOSED ? PROPOSAL_STATUS.APPROVED : PROPOSAL_STATUS.COMPLETED;
-  const acceptLabel = grant.status === PROPOSAL_STATUS.PROPOSED ? "Approve" : "Complete";
-  return (
-    <div className="border p-4 my-4">
-      <h3 className="font-bold">
-        {grant.title}
-        <span className="text-sm text-gray-500 ml-2">({grant.id})</span>
-      </h3>
-      <p>{grant.description}</p>
-      <div className="mt-4">
-        <button
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => reviewGrant(grant, acceptStatus)}
-        >
-          {acceptLabel}
-        </button>
-        <button
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-4"
-          onClick={() => reviewGrant(grant, PROPOSAL_STATUS.REJECTED)}
-        >
-          Reject
-        </button>
-      </div>
-    </div>
-  );
-};
-
 // ToDo. "Protect" with address header or PROTECT with signing the read.
-// ToDo. Loading states (initial, actions, etc)
-// ToDo. Refresh list after action
 const AdminPage = () => {
-  const { address } = useAccount();
-  const [grants, setGrants] = useState<GrantData[]>([]);
-  const { signTypedDataAsync } = useSignTypedData();
-
-  useEffect(() => {
-    const getGrants = async () => {
-      try {
-        const response = await fetch("/api/grants/review");
-        const grants: GrantData[] = (await response.json()).data;
-        setGrants(grants);
-      } catch (error) {
-        notification.error("Error getting grants for review");
-      }
-    };
-
-    getGrants();
-  }, []);
-
-  const reviewGrant = async (grant: GrantData, action: ProposalStatusType) => {
-    let signature;
-    try {
-      signature = await signTypedDataAsync({
-        domain: EIP_712_DOMAIN,
-        types: EIP_712_TYPES__REVIEW_GRANT,
-        primaryType: "Message",
-        message: {
-          grantId: grant.id,
-          action: action,
-        },
-      });
-    } catch (e) {
-      console.error("Error signing message", e);
-      notification.error("Error signing message");
-      return;
-    }
-
-    try {
-      await fetch(`/api/grants/${grant.id}/review`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ signature, signer: address, action }),
-      });
-      notification.success(`Grant reviewed: ${action}`);
-    } catch (error) {
-      notification.error("Error reviewing grant");
-    }
-  };
+  // TODO: Move the response type to a shared location
+  const { data, isLoading } = useSWR<{ data: GrantData[] }>("/api/grants/review", {
+    onError: error => {
+      console.error("Error fetching grants", error);
+      notification.error("Error getting grants data");
+    },
+  });
+  const grants = data?.data;
 
   const completedGrants = grants?.filter(grant => grant.status === PROPOSAL_STATUS.SUBMITTED);
-  const newGrants = grants.filter(grant => grant.status === PROPOSAL_STATUS.PROPOSED);
+  const newGrants = grants?.filter(grant => grant.status === PROPOSAL_STATUS.PROPOSED);
 
   return (
     <div className="container mx-auto max-w-screen-md mt-12">
       <h1 className="text-4xl font-bold">Admin page</h1>
+      {isLoading && <span className="loading loading-spinner "></span>}
       {grants && (
         <>
           <h2 className="font-bold mt-8">Proposals submitted as completed:</h2>
           {completedGrants?.length === 0 && <p>No completed grants</p>}
-          {completedGrants.map(grant => (
-            <GrantReview key={grant.id} grant={grant} reviewGrant={reviewGrant} />
+          {completedGrants?.map(grant => (
+            <GrantReview key={grant.id} grant={grant} />
           ))}
           <h2 className="font-bold mt-8">New grant proposal:</h2>
           {newGrants?.length === 0 && <p>No new grants</p>}
-          {newGrants.map(grant => (
-            <GrantReview key={grant.id} grant={grant} reviewGrant={reviewGrant} />
+          {newGrants?.map(grant => (
+            <GrantReview key={grant.id} grant={grant} />
           ))}
         </>
       )}
