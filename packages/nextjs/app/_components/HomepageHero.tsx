0abcda88d34190328b6103aebd32bc7fb2ef0b0b
Shiv Bhonde <shivbhonde04@gmail.com>
2024-02-23 00:28:22 +0530

    fix width and decrease padding form top nd bottom

--- packages/nextjs/app/_components/HomepageHero.tsx
+++ packages/nextjs/app/_components/HomepageHero.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 
 export const HomepageHero = () => (
-  <div className="container max-w-[90%] lg:max-w-7xl m-auto py-10 lg:py-16 xl:pl-18 lg:pl-8 flex flex-col-reverse lg:flex-row items-center justify-between gap-5 lg:gap-0">
-    <div className="space-y-2 lg:max-w-[65%] flex flex-col items-center lg:items-start">
+  <div className="container max-w-[90%] lg:max-w-7xl m-auto pt-4 pb-8 xl:pl-18 lg:pl-8 flex flex-col-reverse lg:flex-row items-center justify-between gap-5 lg:gap-0">
+    <div className="space-y-2 lg:max-w-[55%] flex flex-col items-center lg:items-start">
       <div className="relative">
         <h2 className="text-3xl md:text-4xl lg:text-6xl lg:leading-[1.2] text-center lg:text-left font-ppEditorial">
           Funding meaningful projects <br /> across the ecosystem
