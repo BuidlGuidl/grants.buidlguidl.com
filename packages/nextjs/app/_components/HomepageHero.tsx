48418107b75718a54f6abf1d759be6b1697bb0b0
Shiv Bhonde <shivbhonde04@gmail.com>
2024-02-23 00:27:44 +0530

    increase planet image size nd add bg to descrip

--- packages/nextjs/app/_components/HomepageHero.tsx
+++ packages/nextjs/app/_components/HomepageHero.tsx
@@ -11,10 +11,12 @@ export const HomepageHero = () => (
         <Image className="absolute -top-3 -right-7" src="/assets/sparkle.png" alt="sparkle" width={32} height={32} />
       </div>
       <div className="text-center font-spaceMono px-1 max-w-lg lg:max-w-none lg:w-4/5 lg:px-0 lg:text-left space-y-5">
-        <p className="m-0 text-xs md:text-sm lg:text-lg">
-          BG grants are one of the ways BuidlGuidl funds development and contribute to the Ethereum ecosystem. Did you
-          just finished SRE or completed one of our batches? This could be the next step in your BuidlGuidl’s journey.
-        </p>
+        <div className="bg-base-300 p-4 rounded-2xl">
+          <p className="m-0 text-xs md:text-sm lg:text-base">
+            BG grants are one of the ways BuidlGuidl funds development and contribute to the Ethereum ecosystem. Did you
+            just finished SRE or completed one of our batches? This could be the next step in your BuidlGuidl’s journey.
+          </p>
+        </div>
         <Link
           href="#communityGrants"
           className="btn btn-primary btn-md border-1 border-black hover:border-black hover:border-1 rounded-2xl px-14 font-bold shadow-none"
@@ -25,7 +27,7 @@ export const HomepageHero = () => (
     </div>
     <div className="flex flex-col items-center">
       <div className="max-w-md lg:max-w-none">
-        <Image src="/assets/bg-universe.png" alt="developers list" width={500} height={500} />
+        <Image src="/assets/bg-universe.png" alt="developers list" width={550} height={550} />
       </div>
     </div>
   </div>
