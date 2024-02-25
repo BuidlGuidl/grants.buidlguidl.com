2bdb8b541784bdcc76ce7c32fd87bd858aa7a9e8
Shiv Bhonde <shivbhonde04@gmail.com>
2024-02-25 17:55:05 +0530

    remove submited grants + count ecosystem grants in total amount

--- packages/nextjs/app/_components/GrantsStats.tsx
+++ packages/nextjs/app/_components/GrantsStats.tsx
@@ -1,8 +1,16 @@
 import Image from "next/image";
+import ecosystemGrants from "~~/services/database/ecosystemGrants.json";
 import { getGrantsStats } from "~~/services/database/grants";
 
 export const GrantsStats = async () => {
   const stats = await getGrantsStats();
+
+  const totalEthGranted = ecosystemGrants.grants.reduce(
+    (acc, grant) => acc + parseFloat(grant.amountGranted),
+    stats.total_eth_granted,
+  );
+  const totalGrants = ecosystemGrants.grants.length + stats.total_grants;
+
   return (
     <div className="bg-base-300">
       <div className="container flex flex-col items-center justify-center max-w-[90%] lg:max-w-7xl mx-auto py-12 lg:px-12 gap-6">
@@ -10,21 +18,14 @@ export const GrantsStats = async () => {
           <div className="flex flex-col items-center">
             <div className="flex gap-3 items-start">
               <Image src="/assets/sparkle.png" alt="diamon icon" width={40} height={40} className="mt-1" />
-              <h2 className="text-4xl lg:text-6xl my-0 font-ppEditorial">{stats.total_completed_grants}</h2>
+              <h2 className="text-4xl lg:text-6xl my-0 font-ppEditorial">{totalGrants}</h2>
             </div>
             <p className="text-lg my-0">Total Grants</p>
           </div>
           <div className="flex flex-col items-center">
             <div className="flex gap-3 items-start">
               <Image src="/assets/sparkle.png" alt="diamon icon" width={40} height={40} className="mt-1" />
-              <h2 className="text-4xl lg:text-6xl my-0 font-ppEditorial">{stats.total_grants}</h2>
-            </div>
-            <p className="text-lg my-0">Submissions</p>
-          </div>
-          <div className="flex flex-col items-center">
-            <div className="flex gap-3 items-start">
-              <Image src="/assets/sparkle.png" alt="diamon icon" width={40} height={40} className="mt-1" />
-              <h2 className="text-4xl lg:text-6xl my-0 font-ppEditorial">{stats.total_eth_granted}</h2>
+              <h2 className="text-4xl lg:text-6xl my-0 font-ppEditorial">{totalEthGranted}</h2>
             </div>
             <p className="text-lg my-0">ETH Granted</p>
           </div>
