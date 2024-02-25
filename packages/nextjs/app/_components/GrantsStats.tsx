69b8a0677509c74d2dadb1ffee332758d5a2a3d9
Shiv Bhonde <shivbhonde04@gmail.com>
2024-02-25 21:39:50 +0530

    update image & round of up to 2 decimals

--- packages/nextjs/app/_components/GrantsStats.tsx
+++ packages/nextjs/app/_components/GrantsStats.tsx
@@ -2,40 +2,36 @@ import Image from "next/image";
 import ecosystemGrants from "~~/services/database/ecosystemGrants.json";
 import { getGrantsStats } from "~~/services/database/grants";
 
+const Stat = ({ label, imgLink, value }: { label: string; imgLink: string; value: string | number }) => {
+  return (
+    <div className="flex flex-col gap-2 items-center">
+      <div className="flex gap-2 items-baseline">
+        <Image src={imgLink} alt={label} width={45} height={50} className="mt-1" />
+        <h2 className="text-4xl lg:text-6xl my-0 font-ppEditorial lg:leading-3">{value}</h2>
+      </div>
+      <p className="text-lg my-0">{label}</p>
+    </div>
+  );
+};
+
 export const GrantsStats = async () => {
   const stats = await getGrantsStats();
 
-  const totalEthGranted = ecosystemGrants.grants.reduce(
+  const sum = ecosystemGrants.grants.reduce(
     (acc, grant) => acc + parseFloat(grant.amountGranted),
     stats.total_eth_granted,
   );
+  const totalEthGranted = Number.isInteger(sum) ? sum : sum.toFixed(2);
+
   const totalGrants = ecosystemGrants.grants.length + stats.total_grants;
 
   return (
     <div className="bg-base-300">
       <div className="container flex flex-col items-center justify-center max-w-[90%] lg:max-w-7xl mx-auto py-12 lg:px-12 gap-6">
         <div className="flex flex-col gap-8 md:flex-row justify-between items-start lg:w-4/5">
-          <div className="flex flex-col items-center">
-            <div className="flex gap-3 items-start">
-              <Image src="/assets/sparkle.png" alt="diamon icon" width={40} height={40} className="mt-1" />
-              <h2 className="text-4xl lg:text-6xl my-0 font-ppEditorial">{totalGrants}</h2>
-            </div>
-            <p className="text-lg my-0">Total Grants</p>
-          </div>
-          <div className="flex flex-col items-center">
-            <div className="flex gap-3 items-start">
-              <Image src="/assets/sparkle.png" alt="diamon icon" width={40} height={40} className="mt-1" />
-              <h2 className="text-4xl lg:text-6xl my-0 font-ppEditorial">{totalEthGranted}</h2>
-            </div>
-            <p className="text-lg my-0">ETH Granted</p>
-          </div>
-          <div className="flex flex-col items-center">
-            <div className="flex gap-3 items-start">
-              <Image src="/assets/sparkle.png" alt="diamon icon" width={40} height={40} className="mt-1" />
-              <h2 className="text-4xl lg:text-6xl my-0 font-ppEditorial">{stats.total_active_grants}</h2>
-            </div>
-            <p className="text-lg my-0">Active Grants</p>
-          </div>
+          <Stat label="Total grants" imgLink="/assets/stats-total.png" value={totalGrants} />
+          <Stat label="ETH granted" imgLink="/assets/stats-eth-granted.png" value={totalEthGranted} />
+          <Stat label="Active grants" imgLink="/assets/stats-active.png" value={stats.total_active_grants} />
         </div>
       </div>
     </div>
