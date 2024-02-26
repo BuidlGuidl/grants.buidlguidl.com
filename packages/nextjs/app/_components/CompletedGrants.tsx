51a656ec4477fffe2b372557bf308576f49340da
Shiv Bhonde <shivbhonde04@gmail.com>
2024-02-26 15:06:11 +0530

    update colors inline with figma

--- packages/nextjs/app/_components/CompletedGrants.tsx
+++ packages/nextjs/app/_components/CompletedGrants.tsx
@@ -4,9 +4,9 @@ import { GrantData } from "~~/services/database/schema";
 
 const CompletedGrantCard = ({ title, description, askAmount }: GrantData) => {
   return (
-    <div className="w-72 md:w-72 bg-base-200 min-h-full rounded-2xl overflow-hidden shadow-lg p-4 space-y-4">
-      <div className="h-44 w-full bg-secondary rounded-2xl relative">
-        <div className="badge bg-base-200 absolute top-4 right-4 ">Learn more</div>
+    <div className="w-72 md:w-72 bg-primary min-h-full rounded-3xl overflow-hidden shadow-lg p-4 space-y-4">
+      <div className="h-44 w-full bg-secondary rounded-3xl relative">
+        <div className="badge bg-base-200 absolute top-4 right-4 rounded-3xl py-3 px-3">Learn more</div>
         <p className="m-0 absolute bottom-4 left-4 text-lg">{title}</p>
       </div>
       <div className="space-y-2">
@@ -20,17 +20,11 @@ export const CompletedGrants = async () => {
   const completedGrants = await getAllCompletedGrants();
 
   return (
-    <div className="bg-primary">
+    <div className="bg-base-100">
       <div className="container flex flex-col justify-center max-w-[95%] lg:max-w-7xl mx-auto py-12 gap-4 border border-red-400">
         <div className="self-center lg:self-start w-fit relative">
           <h2 className="text-4xl lg:text-6xl text-center lg:text-left font-ppEditorial">Completed grants</h2>
-          <Image
-            className="absolute -top-3 -right-7 invert"
-            src="/assets/sparkle.png"
-            alt="sparkle"
-            width={32}
-            height={32}
-          />
+          <Image className="absolute -top-3 -right-7" src="/assets/sparkle.png" alt="sparkle" width={32} height={32} />
         </div>
         <div className="flex flex-col items-center justify-center md:flex-row md:flex-wrap md:items-start gap-8">
           {completedGrants.map(grant => (
