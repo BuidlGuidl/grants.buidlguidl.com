69bca900b0dbc5aad3dbee5f344c1fb613871e12
Shiv Bhonde <shivbhonde04@gmail.com>
2024-02-26 15:27:13 +0530

    add builder address to card ad move Amount eth up

--- packages/nextjs/app/_components/CompletedGrants.tsx
+++ packages/nextjs/app/_components/CompletedGrants.tsx
@@ -1,17 +1,21 @@
 import Image from "next/image";
+import { Address } from "~~/components/scaffold-eth";
 import { getAllCompletedGrants } from "~~/services/database/grants";
 import { GrantData } from "~~/services/database/schema";
 
-const CompletedGrantCard = ({ title, description, askAmount }: GrantData) => {
+const CompletedGrantCard = ({ title, description, askAmount, builder }: GrantData) => {
   return (
-    <div className="w-72 md:w-72 bg-primary min-h-full rounded-3xl overflow-hidden shadow-lg p-4 space-y-4">
-      <div className="h-44 w-full bg-secondary rounded-3xl relative">
-        <div className="badge bg-base-200 absolute top-4 right-4 rounded-3xl py-3 px-3">Learn more</div>
+    <div className="w-72 md:w-[295px] bg-primary min-h-full rounded-2xl overflow-hidden shadow-lg p-4 space-y-4">
+      <div className="h-44 w-full bg-secondary rounded-xl relative">
+        <div className="badge bg-base-200 absolute top-4 right-4 rounded-2xl py-3 px-3">Learn more</div>
         <p className="m-0 absolute bottom-4 left-4 text-lg">{title}</p>
       </div>
-      <div className="space-y-2">
-        <p className="text-base m-0 line-clamp-4 hover:line-clamp-none">{description}</p>
-        <p className="text-base font-medium m-0">Amounted Granted: {askAmount} ETH</p>
+      <div className="flex flex-col gap-2 px-3">
+        <Address address={builder} />
+        <p className="text-base m-0 mt-2">
+          Amount granted: <span className="font-medium"> {askAmount} ETH</span>
+        </p>
+        <p className="text-base m-0 line-clamp-3 hover:line-clamp-none">{description}</p>
       </div>
     </div>
   );
@@ -21,7 +25,7 @@ export const CompletedGrants = async () => {
 
   return (
     <div className="bg-base-100">
-      <div className="container flex flex-col justify-center max-w-[95%] lg:max-w-7xl mx-auto py-12 gap-4 border border-red-400">
+      <div className="container flex flex-col justify-center max-w-[95%] lg:max-w-7xl mx-auto py-12 gap-4">
         <div className="self-center lg:self-start w-fit relative">
           <h2 className="text-4xl lg:text-6xl text-center lg:text-left font-ppEditorial">Completed grants</h2>
           <Image className="absolute -top-3 -right-7" src="/assets/sparkle.png" alt="sparkle" width={32} height={32} />
