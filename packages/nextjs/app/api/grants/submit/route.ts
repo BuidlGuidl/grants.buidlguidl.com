8153797fa25d3351f7d0cd2a3a82398af2c61901
Shiv Bhonde <shivbhonde04@gmail.com>
2024-02-29 19:03:49 +0530

    add todo about verifying submitted build link

--- packages/nextjs/app/api/grants/submit/route.ts
+++ packages/nextjs/app/api/grants/submit/route.ts
@@ -11,6 +11,8 @@ type ReqBody = {
   signature?: `0x${string}`;
 };
 
+// TODO: Maybe we could fetch build from firebase and check if its really present,
+// also check that build was actually submitted by the same builder
 export async function POST(req: Request) {
   try {
     const { grantId, link, signer, signature } = (await req.json()) as ReqBody;
