ce1e6a4b6fc52a516dbe54dae2f73f9841ad4849
Shiv Bhonde <shivbhonde04@gmail.com>
2024-03-05 23:30:08 +0530

    use force-dynamic to make grants api dynamic

--- packages/nextjs/app/api/grants/review/route.tsx
+++ packages/nextjs/app/api/grants/review/route.tsx
@@ -1,12 +1,11 @@
-import { revalidatePath } from "next/cache";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { getAllGrantsForReview } from "~~/services/database/grants";
 
-export async function GET(request: NextRequest) {
+export const dynamic = "force-dynamic";
+
+export async function GET() {
   try {
-    const path = request.nextUrl.searchParams.get("path");
     const grants = await getAllGrantsForReview();
-    if (path) revalidatePath(path);
     return NextResponse.json({ data: grants });
   } catch (error) {
     return NextResponse.json(
