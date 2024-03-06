864ae4f545635cb766aa957f68a690c13a5669bb
Carlos Sánchez <oceanrdn@gmail.com>
2024-03-06 12:20:49 +0100

    Add dummy contract

---
+++ packages/hardhat/contracts/BGGrants.sol
@@ -0,0 +1,57 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+// Dummy contract for initial iteration
+// From https://github.com/BuidlGuidl/Eth-Splitter/blob/master/packages/hardhat/contracts/Splitter.sol
+contract BGGrants is Ownable, ReentrancyGuard {
+    error INVALID_RECIPIENT();
+    error INSUFFICIENT_RECIPIENT_COUNT();
+    error TRANSFER_FAILED();
+
+    event EthSplitEqual(address indexed sender, uint256 totalAmount, address payable[] recipients);
+
+    constructor(address _owner) {
+        super.transferOwnership(_owner);
+    }
+
+    function splitEqualETH(address payable[] calldata recipients) external payable nonReentrant {
+        uint256 totalAmount = msg.value;
+        uint256 rLength = recipients.length;
+        uint256 equalAmount = totalAmount / rLength;
+        uint256 remainingAmount = totalAmount % rLength;
+
+        if (rLength > 25 || rLength < 2) revert INSUFFICIENT_RECIPIENT_COUNT();
+
+        for (uint256 i = 0; i < rLength; ) {
+            if (recipients[i] == address(0)) revert INVALID_RECIPIENT();
+            uint256 amountToSend = equalAmount;
+            if (i == 0) {
+                amountToSend = amountToSend + remainingAmount;
+            }
+            (bool success, ) = recipients[i].call{value: amountToSend, gas: 20000}("");
+            if (!success) revert TRANSFER_FAILED();
+            unchecked {
+                ++i;
+            }
+        }
+
+        emit EthSplitEqual(msg.sender, msg.value, recipients);
+    }
+
+    /**
+     * Function that allows the owner to withdraw all the Ether in the contract
+     * The function can only be called by the owner of the contract as defined by the isOwner modifier
+     */
+    function withdraw() public onlyOwner {
+        (bool success, ) = owner().call{ value: address(this).balance }("");
+        require(success, "Failed to send Ether");
+    }
+
+    /**
+     * Function that allows the contract to receive ETH
+     */
+    receive() external payable {}
+}
--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -1,6 +1,5 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import { Contract } from "ethers";
 
 /**
  * Deploys a contract named "YourContract" using the deployer account and
@@ -22,7 +21,7 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  await deploy("YourContract", {
+  await deploy("BGGrants", {
     from: deployer,
     // Contract constructor arguments
     args: [deployer],
@@ -31,14 +30,10 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
     // automatically mining the contract deployment transaction. There is no effect on live networks.
     autoMine: true,
   });
-
-  // Get the deployed contract to interact with it after deploying.
-  const yourContract = await hre.ethers.getContract<Contract>("YourContract", deployer);
-  console.log("👋 Initial greeting:", await yourContract.greeting());
 };
 
 export default deployYourContract;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags YourContract
-deployYourContract.tags = ["YourContract"];
+deployYourContract.tags = ["BGGrants"];
