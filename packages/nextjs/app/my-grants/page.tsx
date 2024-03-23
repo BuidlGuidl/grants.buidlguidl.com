0769f95cc250d5d87f377c1ab948992574f53509
Pablo Alayeto <55535804+Pabl0cks@users.noreply.github.com>
2024-03-24 00:45:29 +0100

    Add payment tx links in /my-grants

--- packages/nextjs/app/my-grants/page.tsx
+++ packages/nextjs/app/my-grants/page.tsx
@@ -6,6 +6,7 @@ import { SubmitModal } from "./_components/SubmitModal";
 import { NextPage } from "next";
 import useSWR from "swr";
 import { useAccount } from "wagmi";
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid";
 import { GrantData } from "~~/services/database/schema";
 import { PROPOSAL_STATUS } from "~~/utils/grants";
 
@@ -50,6 +51,29 @@ const MyGrants: NextPage = () => {
           )}
           <p className="m-0">{grant.description}</p>
           <p className={`badge ${badgeBgColor[grant.status]}`}>{grant.status}</p>
+          {(grant.status === PROPOSAL_STATUS.APPROVED ||
+            grant.status === PROPOSAL_STATUS.SUBMITTED ||
+            grant.status === PROPOSAL_STATUS.COMPLETED) &&
+            grant.approvedTx && (
+              <a
+                href={grant.approvedTx}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="ml-4 underline underline-offset-4 text-xs"
+              >
+                50% approve tx <ArrowTopRightOnSquareIcon className="h-4 w-4 inline" />
+              </a>
+            )}
+          {grant.status === PROPOSAL_STATUS.COMPLETED && grant.completedTx && (
+            <a
+              href={grant.completedTx}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="ml-4 underline underline-offset-4 text-xs"
+            >
+              50% complete tx <ArrowTopRightOnSquareIcon className="h-4 w-4 inline" />
+            </a>
+          )}
           {grant.status === PROPOSAL_STATUS.APPROVED && (
             <button onClick={() => openModal(grant)} className="btn btn-primary float-right">
               Submit build
